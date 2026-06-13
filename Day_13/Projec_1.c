cd2d2bd27defe9d7c7e545178cad5118d0264329
Shubh Gupta <shubhgupta3692005@gmail.com>
2026-06-13 12:52:06 +0530

    first commit

---
+++ Day_13/Projec_1.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main() 
+{
+    int n, i;
+    printf("Enter the number of elements: ");
+    scanf("%d", &n);
+    int arr[n];
+    printf("Enter %d elements:\n", n);
+    for (i = 0; i < n; i++) 
+    {
+        printf("Element %d: ", i + 1);
+        scanf("%d", &arr[i]);
+    }
+    printf("\nThe elements in the array are: ");
+    for (i = 0; i < n; i++) 
+        printf("%d ", arr[i]);
+    printf("\n");
+    return 0;
+}
