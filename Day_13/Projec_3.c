71e83a70ef60cbc0708a851bf374d071af3c81db
Shubh Gupta <shubhgupta3692005@gmail.com>
2026-06-13 13:13:49 +0530

    first commit

---
+++ Day_13/Projec_3.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+int main() 
+{
+     int i, n , largest, smallest;
+    printf("Enter the number of elements in the array: ");
+    scan("%d",&n);
+    int arr[n];
+    printf("Enter the elements: ");
+    for (i = 0; i < n; i++) 
+       scanf("%d", &arr[i]);
+    largest = arr[0];
+    smallest = arr[0];
+    for (i = 1; i < n; i++) 
+    {
+        if (arr[i] > largest) 
+            largest = arr[i];
+        if (arr[i] < smallest) 
+            smallest = arr[i];
+    }
+    printf("Largest element = %d\n", largest);
+    printf("Smallest element = %d\n", smallest);
+    return 0;
+}
