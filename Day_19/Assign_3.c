8b7a344070e13a160dfd8b33b0e52c3c8a5a0f77
Shubh Gupta <shubhgupta3692005@gmail.com>
2026-06-16 15:09:05 +0530

    first commit

---
+++ Day_19/Assign_3.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+int main() 
+{
+    int rows, cols;
+    printf("Enter number of rows and columns: ");
+    scanf("%d %d", &rows, &cols);
+    int matrix[rows][cols];
+    int transpose[cols][rows];
+    printf("\nEnter elements of the matrix:\n");
+    for (int i = 0; i < rows; i++) 
+    {
+        for (int j = 0; j < cols; j++) 
+        {
+            printf("Enter element [%d][%d]: ", i, j);
+            scanf("%d", &matrix[i][j]);
+        }
+    }
+    for (int i = 0; i < rows; i++) 
+    {
+        for (int j = 0; j < cols; j++) 
+            transpose[j][i] = matrix[i][j];
+    }
+    printf("\nTransposed Matrix:\n");
+    for (int i = 0; i < cols; i++) 
+    {
+        for (int j = 0; j < rows; j++) 
+            printf("%d ", transpose[i][j]);
+        printf("\n");
+    }
+    return 0;
+}
